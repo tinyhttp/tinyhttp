c459c6f60e635e28a1338ed66e73e8e4cf92d70b
Alex <alex190030@gmail.com>
2020-07-18 18:01:29 +0300

    Moved test to mw folder

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -160,28 +160,4 @@ describe('Testing routes', () => {
         done()
       })
   })
-
-  it('should use the logger middleware', (done) => {
-    const originalConsoleLog = console.log;
-
-    console.log = (log) => {
-      expect(log.split(' ')[2]).toBe('GET');
-      console.log = originalConsoleLog;
-      done();
-    }
-    
-    const app = new App()
-    app.use(logger({ timestamp: { format: 'HH:mm:ss' } }))
-
-    const server = app.listen()
-
-    const request: any = supertest(server)
-
-    request
-      .get('/')
-      .expect(404)
-      .end(() => {
-        server.close()
-      })
-  })
 })
