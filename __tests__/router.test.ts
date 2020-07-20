483375109c16d42227d56cd9a6f0d64d78961943
elianiva <dicha.arkana03@gmail.com>
2020-07-21 06:15:56 +0700

    added some route tests

--- __tests__/router.test.ts
+++ __tests__/router.test.ts
@@ -99,3 +99,346 @@ describe('Testing Router', () => {
       })
   })
 })
+
+describe('Route methods', () => {
+  it('app.get handles get request', (done) => {
+    const app = new App()
+
+    app.get('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .get('/')
+      .expect(200, 'GET')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.post handles post request', (done) => {
+    const app = new App()
+
+    app.post('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .post('/')
+      .expect(200, 'POST')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.put handles put request', (done) => {
+    const app = new App()
+
+    app.put('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .put('/')
+      .expect(200, 'PUT')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.patch handles patch request', (done) => {
+    const app = new App()
+
+    app.patch('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .patch('/')
+      .expect(200, 'PATCH')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.head handles head request', (done) => {
+    const app = new App()
+
+    app.head('/', (req, res) => void res.end(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .head('/')
+      .expect(200, '' || undefined)
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.delete handles delete request', (done) => {
+    const app = new App()
+
+    app.delete('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .delete('/')
+      .expect(200, 'DELETE')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.checkout handles checkout request', (done) => {
+    const app = new App()
+
+    app.checkout('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .checkout('/')
+      .expect(200, 'CHECKOUT')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.copy handles copy request', (done) => {
+    const app = new App()
+
+    app.copy('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .copy('/')
+      .expect(200, 'COPY')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.lock handles lock request', (done) => {
+    const app = new App()
+
+    app.lock('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .lock('/')
+      .expect(200, 'LOCK')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.merge handles merge request', (done) => {
+    const app = new App()
+
+    app.merge('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .merge('/')
+      .expect(200, 'MERGE')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.mkactivity handles mkactivity request', (done) => {
+    const app = new App()
+
+    app.mkactivity('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .mkactivity('/')
+      .expect(200, 'MKACTIVITY')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.mkcol handles mkcol request', (done) => {
+    const app = new App()
+
+    app.mkcol('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .mkcol('/')
+      .expect(200, 'MKCOL')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.move handles move request', (done) => {
+    const app = new App()
+
+    app.move('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .move('/')
+      .expect(200, 'MOVE')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.search handles search request', (done) => {
+    const app = new App()
+
+    app.search('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .search('/')
+      .expect(200, 'SEARCH')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.notify handles notify request', (done) => {
+    const app = new App()
+
+    app.notify('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .notify('/')
+      .expect(200, 'NOTIFY')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.purge handles purge request', (done) => {
+    const app = new App()
+
+    app.purge('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .purge('/')
+      .expect(200, 'PURGE')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.report handles report request', (done) => {
+    const app = new App()
+
+    app.report('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .report('/')
+      .expect(200, 'REPORT')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.subscribe handles subscribe request', (done) => {
+    const app = new App()
+
+    app.subscribe('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .subscribe('/')
+      .expect(200, 'SUBSCRIBE')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.unsubscribe handles unsubscribe request', (done) => {
+    const app = new App()
+
+    app.unsubscribe('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .unsubscribe('/')
+      .expect(200, 'UNSUBSCRIBE')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+  it('app.trace handles trace request', (done) => {
+    const app = new App()
+
+    app.trace('/', (req, res) => void res.send(req.method))
+
+    const server = app.listen()
+    const request = supertest(server)
+
+    request
+      .trace('/')
+      .expect(200, 'TRACE')
+      .end((err: Error) => {
+        server.close()
+        if (err) return done(err)
+        done()
+      })
+  })
+})
