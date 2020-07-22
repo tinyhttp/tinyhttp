b77b77c259dee25eddd7d815a10906c5e7439a10
elianiva <dicha.arkana03@gmail.com>
2020-07-22 21:12:55 +0700

    added next-custom-server-example

---
+++ examples/next-custom-server/blogs/index.js
@@ -0,0 +1,6 @@
+export default [
+  { slug: 'something-epic', title: 'Something Epic', desc: 'This is an example of tinyhttp for nextjs custom server.' },
+  { slug: 'awesome-post', title: 'Awesome Stuff', desc: 'This is an example of tinyhttp for nextjs custom server.' },
+  { slug: 'yet-another-awesome-post', title: 'Something Awesome', desc: 'This is an example of tinyhttp for nextjs custom server.' },
+  { slug: 'lorem ipsum', title: 'Lorem Ipsum', desc: 'Consectetur expedita ex repellat quibusdam esse eveniet Doloremque nesciunt corporis.' },
+]
