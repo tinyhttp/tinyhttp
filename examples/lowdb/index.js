6495a8cec12d8e65c60cdd3a17946225ec44cfca
v1rtl <pilll.pl22@gmail.com>
2020-08-13 00:52:46 +0300

    improve LowDB example

--- examples/lowdb/index.js
+++ examples/lowdb/index.js
@@ -1,14 +1,18 @@
-const low = require('lowdb')
-const FileSync = require('lowdb/adapters/FileSync')
-const { App } = require('@tinyhttp/app')
+import low from 'lowdb'
+import FileSync from 'lowdb/adapters/FileSync.js'
+import { App } from '@tinyhttp/app'
+import { form as parser } from 'body-parsec'
 
 const app = new App()
 const adapter = new FileSync('db.json')
 const db = low(adapter)
 
 // get all posts
-app.get('/', (req, res) => {
-  res.send(db.get('posts'))
+
+app.use(parser())
+
+app.get('/', (_, res) => {
+  res.send(db.getState().posts)
 })
 
 // get post by id
@@ -16,6 +20,7 @@ app.get('/:id', (req, res) => {
   res.send(
     db
       .get('posts')
+      // @ts-ignore
       .find({ id: parseInt(req.params.id) })
       .value()
   )
@@ -23,14 +28,19 @@ app.get('/:id', (req, res) => {
 
 // add a post
 app.post('/', (req, res) => {
-  db.get('posts').push({ id: Date.now(), title: req.query.title, likes: 0 }).write()
-  res.send({ msg: `Post with title of "${req.query.title}" is successfully added` })
+  if (req.body.title) {
+    db.get('posts').push({ id: Date.now(), title: req.body.title, likes: 0 }).write()
+    res.send({ msg: `Post with title of "${req.body.title}" is successfully added` })
+  } else {
+    res.send('Post title missing')
+  }
 })
 
 // like a post
 app.put('/:id', (req, res) => {
   const currentPost = db
     .get('posts')
+    // @ts-ignore
     .find({ id: parseInt(req.params.id) })
     .value()
   currentPost.likes += 1
@@ -41,6 +51,7 @@ app.put('/:id', (req, res) => {
 // delete a post
 app.delete('/:id', (req, res) => {
   db.get('posts')
+    // @ts-ignore
     .remove({ id: parseInt(req.params.id) })
     .write()
   res.send({ msg: `A post with an id of ${req.params.id} has been deleted` })
