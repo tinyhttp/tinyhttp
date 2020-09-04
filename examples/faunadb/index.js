cbdece114eb7289efb4a4de061a8cbadf3457f16
v1rtl <pilll.pl22@gmail.com>
2020-09-04 21:21:05 +0300

    Add FaunaDB example

---
+++ examples/faunadb/index.js
@@ -0,0 +1,75 @@
+import { App } from '@tinyhttp/app'
+import * as dotenv from '@tinyhttp/dotenv'
+import fauna from 'faunadb'
+import { json } from 'body-parsec'
+
+const isEmptyObject = (obj) => Object.keys(obj).length === 0 && obj.constructor === Object
+
+const hasPostProps = (obj) => Object.keys(obj).every((x) => ['name', 'description', 'price', 'quantity'].includes(x))
+
+const q = fauna.query
+
+dotenv.config()
+
+const client = new fauna.Client({
+  secret: process.env.DB_KEY,
+})
+
+const app = new App()
+
+app.use(json())
+
+app.get('/products', async (_, res) => {
+  const query = await client.query(
+    q.Map(
+      // iterate each item in result
+      q.Paginate(
+        // make paginatable
+        q.Match(
+          // query index
+          q.Index('all_products') // specify source
+        )
+      ),
+      (ref) => q.Get(ref) // lookup each result by its reference
+    )
+  )
+
+  // @ts-ignore
+  res.send(query.data)
+})
+
+app.get('/product/:name', async (req, res, next) => {
+  const name = req.params.name
+
+  if (!name) {
+    res.sendStatus(404)
+  } else {
+    try {
+      const query = await client.query(q.Get(q.Match(q.Index('products_by_name'), name)))
+      // @ts-ignore
+      res.send(query.data)
+    } catch (e) {
+      console.log(e)
+      next(e)
+    }
+  }
+})
+
+app.post('/product', async (req, res, next) => {
+  if (!isEmptyObject(req.body) && hasPostProps(req.body)) {
+    try {
+      const query = await client.query(
+        q.Create(q.Collection('products'), {
+          data: req.body,
+        })
+      )
+
+      res.send(query)
+    } catch (e) {
+      console.log(e)
+      next(e)
+    }
+  }
+})
+
+app.listen(3000, () => console.log(`Started on http://localhost:3000`))
--- examples/mongodb/README.md
+++ examples/mongodb/README.md
@@ -12,7 +12,7 @@ pnpm install
 
 ### Create a database
 
-Go to [MongoDB Atlas](https://www.mongodb.com/cloud/atlas), create a new database and grab the URI string, then put it in the `.env` file.
+Go to [MongoDB Atlas](https://www.mongodb.com/cloud/atlas), create a new database and grab the URI string, then put `DB_URI=<URI>` in the `.env` file.
 
 ## Run
 
@@ -22,10 +22,10 @@ node index.js
 
 ## Endpoints
 
-`GET /notes` will return a list of notes with 2 properties which are title and desc.
+- `GET /notes` - list notes with 2 properties which are title and desc.
 
-`POST /notes` will add a post using the data from `title` and `desc` query
+- `POST /notes` - create a post using the data from `title` and `desc` query
 
-`DELETE /notes` will delete a note with specified id
+- `DELETE /notes` - delete a note with specified ID
 
-`PUT /notes` requires and ID of the item that you want to update as well as the title and desc for the updated one.
+- `PUT /notes` - update a note by ID
