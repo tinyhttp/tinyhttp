bf9963bd06b416b4099cef8be049a13dd7f448d2
ionel <ionel.cristian.lupu@gmail.com>
2020-09-11 19:58:00 +0300

    reformatted code using prettier

--- examples/typescript-decorators/index.ts
+++ examples/typescript-decorators/index.ts
@@ -3,12 +3,10 @@ import { basePath, get, register } from 'express-decorators';
 
 @basePath('/hello')
 class TestController {
-
-    @get('/world')
-    async sayHelloAction(request: Request, response: Response) {
-        response.send('<h1>Hello World</h1>');
-    }
-
+  @get('/world')
+  async sayHelloAction(request: Request, response: Response) {
+    response.send('<h1>Hello World</h1>');
+  }
 }
 
 const app = new App();
@@ -16,5 +14,3 @@ const app = new App();
 register(app, new TestController());
 
 app.listen(3000, () => console.log('Started on http://localhost:3000'));
-
-
