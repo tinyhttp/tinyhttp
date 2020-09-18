8206006104db602694e89dc3fff776c4030db547
BRA1L0R <kapakapa197@gmail.com>
2020-09-18 15:44:53 +0200

    Added more tests for the JWT module

--- __tests__/wares/jwt.test.ts
+++ __tests__/wares/jwt.test.ts
@@ -7,12 +7,17 @@ describe('JsonWebToken tests', () => {
   const req = {} as Request
   const res = {} as Response
 
+  beforeEach(() => {
+    req.headers = {}
+    req.user = {}
+  })
+
   it('should work if authorization header is valid jsonwebtoken', () => {
     const secret = 'shhhhhh'
     const token = jsonwebtoken.sign({ foo: 'bar' }, secret)
 
-    req.headers = {}
     req.headers.authorization = 'Bearer ' + token
+
     jwt({ secret: secret, algorithm: 'HS256' })(req, res, () => {
       expect('bar').toBe(req.user.foo)
     })
@@ -22,8 +27,8 @@ describe('JsonWebToken tests', () => {
     const secret = Buffer.from('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'base64')
     const token = jsonwebtoken.sign({ foo: 'bar' }, secret)
 
-    req.headers = {}
     req.headers.authorization = 'Bearer ' + token
+
     jwt({ secret: secret.toString(), algorithm: 'HS256' })(req, res, () => {
       expect('bar').toBe(req.user.foo)
     })
@@ -33,11 +38,22 @@ describe('JsonWebToken tests', () => {
     const privateKey = fs.readFileSync('__tests__/fixtures/jwt/private', { encoding: 'utf-8' })
     const publicKey = fs.readFileSync('__tests__/fixtures/jwt/public', { encoding: 'utf-8' })
 
-    req.headers = {}
     req.headers.authorization = 'Bearer ' + jsonwebtoken.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' })
 
     jwt({ secret: [privateKey, publicKey], algorithm: 'RS256' })(req, res, () => {
       expect('bar').toBe(req.user.foo)
+    })
+  })
+
+  it('should not work with malformed input', () => {
+    const fake_secret = 'notmuchsecret'
+    const true_secret = 'verysecret'
+    const token = jsonwebtoken.sign({ foo: 'bar' }, fake_secret)
+
+    req.headers.authorization = 'Bearer ' + token
+
+    jwt({ secret: true_secret, algorithm: 'HS256' })(req, res, () => {
+      expect(req.user.foo).toBeUndefined()
     })
   })
 })
