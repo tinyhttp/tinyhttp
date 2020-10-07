48230ec782643e4df11f548051b8385548b07e36
shzmr <shzmrdev@gmail.com>
2020-10-07 18:52:09 +0000

    Added sapper example

---
+++ examples/sapper/src/routes/about.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this example</h1>
+
+<p>This example uses tinyhttp and sapper.</p>
+
+<ul>
+    <li>
+        <a href="https://tinyhttp.v1rtl.site/docs">tinyhttp documentation</a>
+    </li>
+    <li>
+        <a href="https://sapper.svelte.dev/docs/">sapper documentation</a>
+    </li>
+</ul>
