0eec8f760cd67324df35c0668524544eef8dc126
Maurizio Pozzobon <mauriziopz@gmail.com>
2020-10-08 22:13:01 +0200

    #140 added tests for req

--- __tests__/modules/req.test.ts
+++ __tests__/modules/req.test.ts
@@ -1,5 +1,6 @@
 import { makeFetch } from 'supertest-fetch'
-import { checkIfXMLHttpRequest, getAccepts, getFreshOrStale, getRequestHeader } from '../../packages/req/src'
+import { checkIfXMLHttpRequest, getAccepts, getFreshOrStale, getRequestHeader, getRangeFromHeader, reqIs } from '../../packages/req/src'
+import { Ranges } from 'range-parser'
 import { runServer } from '../../test_helpers/runServer'
 
 describe('Request extensions', () => {
@@ -85,5 +86,187 @@ describe('Request extensions', () => {
     })
 
     await makeFetch(app)('/').expect('rotten')
+  })
+
+  describe('req.range', () => {
+    it('should return parsed ranges', async () => {
+      const app = runServer((req, res) => {
+        const range = getRangeFromHeader(req)
+        const array = range(300)
+        expect(array).toContainEqual({ end: 299, start: 0 })
+        expect(array).toHaveLength(1)
+        res.end()
+      })
+
+      await makeFetch(app)('/', {
+        headers: {
+          Range: 'bytes=0-1000',
+        },
+      })
+      expect.assertions(2)
+    })
+    it('should cap to the given size', async () => {
+      const app = runServer((req, res) => {
+        const range = getRangeFromHeader(req)
+        const size = 300
+        expect(range(size)[0].end).toBe(size - 1)
+        res.end()
+      })
+
+      await makeFetch(app)('/', {
+        headers: {
+          Range: 'bytes=0-1000',
+        },
+      })
+      expect.assertions(1)
+    })
+    it('should cap to the given size when open-ended', async () => {
+      const app = runServer((req, res) => {
+        const range = getRangeFromHeader(req)
+        const size = 300
+        expect(range(size)[0].end).toBe(size - 1)
+        res.end()
+      })
+
+      await makeFetch(app)('/', {
+        headers: {
+          Range: 'bytes=0-',
+        },
+      })
+      expect.assertions(1)
+    })
+    it('should have a .type', async () => {
+      const app = runServer((req, res) => {
+        const range = getRangeFromHeader(req)
+        expect((range(300) as Ranges).type).toBe('bytes')
+        res.end()
+      })
+
+      await makeFetch(app)('/', {
+        headers: {
+          Range: 'bytes=0-1000',
+        },
+      })
+      expect.assertions(1)
+    })
+    it('should accept any type', async () => {
+      const app = runServer((req, res) => {
+        const range = getRangeFromHeader(req)
+        expect((range(300) as Ranges).type).toBe('any')
+        res.end()
+      })
+
+      await makeFetch(app)('/', {
+        headers: {
+          Range: 'any=0-1000',
+        },
+      })
+      expect.assertions(1)
+    })
+    it('should return undefined if no range', async () => {
+      const app = runServer((req, res) => {
+        const range = getRangeFromHeader(req)
+        expect(range(300)).toBeUndefined()
+        res.end()
+      })
+
+      await makeFetch(app)('/')
+      expect.assertions(1)
+    })
+    describe('with options', () => {
+      it('should return combined ranges if combine set to true', async () => {
+        const app = runServer((req, res) => {
+          const range = getRangeFromHeader(req)
+          const array = range(300, { combine: true })
+          expect(array).toContainEqual({ end: 299, start: 0 })
+          expect(array).toHaveLength(1)
+          res.end()
+        })
+
+        await makeFetch(app)('/', {
+          headers: {
+            Range: 'bytes=0-100, 101-500',
+          },
+        })
+        expect.assertions(2)
+      })
+      it('should return separated ranges if combine set to false', async () => {
+        const app = runServer((req, res) => {
+          const range = getRangeFromHeader(req)
+          const array = range(300, { combine: false })
+          expect(array).toContainEqual({ end: 100, start: 0 })
+          expect(array).toContainEqual({ end: 299, start: 101 })
+          expect(array).toHaveLength(2)
+          res.end()
+        })
+
+        await makeFetch(app)('/', {
+          headers: {
+            Range: 'bytes=0-100, 101-500',
+          },
+        })
+        expect.assertions(3)
+      })
+    })
+  })
+  describe('req.is', () => {
+    it('should return the given MIME type when matching', async () => {
+      const app = runServer((req, res) => {
+        expect(reqIs(req)('text/plain')).toBe('text/plain')
+        res.end()
+      })
+      await makeFetch(app)('/', {
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+      })
+      expect.assertions(1)
+    })
+    it('should return false when not matching', async () => {
+      const app = runServer((req, res) => {
+        expect(reqIs(req)('text/other')).toBe(false)
+        res.end()
+      })
+      await makeFetch(app)('/', {
+        headers: {
+          'Content-Type': 'text/plain',
+        },
+      })
+      expect.assertions(1)
+    })
+    it('should return false when Content-Type header is not present', async () => {
+      const app = runServer((req, res) => {
+        expect(reqIs(req)('text/other')).toBe(false)
+        res.end()
+      })
+      await makeFetch(app)('/', {
+        headers: {},
+      })
+      expect.assertions(1)
+    })
+    it("Should lookup the MIME type with the extension given (e.g. req.is('json')", async () => {
+      const app = runServer((req, res) => {
+        expect(reqIs(req)('json')).toBe('json')
+        res.end()
+      })
+      await makeFetch(app)('/', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      expect.assertions(1)
+    })
+    xit('should ignore charset', async () => {
+      const app = runServer((req, res) => {
+        expect(reqIs(req)('text/html')).toBe('text/html')
+        res.end()
+      })
+      await makeFetch(app)('/', {
+        headers: {
+          'Content-Type': 'text/html; charset=UTF-8',
+        },
+      })
+      expect.assertions(1)
+    })
   })
 })
