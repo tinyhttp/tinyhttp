e3921e11fdebf9b468e4ffa72cdbf02b4d010e88
Kolja Markwardt <kolja.markwardt@edeka.de>
2020-10-16 22:30:23 +0200

    moved pug file to fixtures folder

--- __tests__/wares/pug.test.ts
+++ __tests__/wares/pug.test.ts
@@ -5,7 +5,9 @@ import { renderFile } from 'pug'
 
 describe('PUG templates tests', () => {
   it('should render pug template', async () => {
-    const { fetch, app } = InitAppAndTest((_req, res) => void res.render('../__tests__/wares/pug.test.pug', null, { renderOptions: { pageTitle: 'Test title', foo: 'bar' } }))
+    const { fetch, app } = InitAppAndTest(
+      (_req, res) => void res.render('../__tests__/fixtures/views/pug.test.pug', null, { renderOptions: { pageTitle: 'Test title', foo: 'bar' } })
+    )
     pug()(app)
 
     await fetch('/').expect(200, '<!DOCTYPE html><html lang="en"><head><title>Test title</title></head><body><h1>Pug bar</h1></body></html>')
