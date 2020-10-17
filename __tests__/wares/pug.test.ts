9c2720e9d98984833a35588ffba145cbf4757425
Kolja Markwardt <kolja.markwardt@edeka.de>
2020-10-17 07:19:19 +0200

    renamed test file

--- __tests__/wares/pug.test.ts
+++ __tests__/wares/pug.test.ts
@@ -3,11 +3,9 @@ import { createServer } from 'http'
 import { InitAppAndTest } from '../../test_helpers/initAppAndTest'
 import { renderFile } from 'pug'
 
-describe('PUG templates tests', () => {
+describe('Pug templates tests', () => {
   it('should render pug template', async () => {
-    const { fetch, app } = InitAppAndTest(
-      (_req, res) => void res.render('../__tests__/fixtures/views/pug.test.pug', null, { renderOptions: { pageTitle: 'Test title', foo: 'bar' } })
-    )
+    const { fetch, app } = InitAppAndTest((_req, res) => void res.render('../__tests__/fixtures/views/test.pug', null, { renderOptions: { pageTitle: 'Test title', foo: 'bar' } }))
     pug()(app)
 
     await fetch('/').expect(200, '<!DOCTYPE html><html lang="en"><head><title>Test title</title></head><body><h1>Pug bar</h1></body></html>')
