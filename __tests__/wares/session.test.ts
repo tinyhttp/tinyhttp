5e21a30db1e6177b6ad5d046102e3e48e4d573fa
Alan Carpilovsky <8486092+alcar@users.noreply.github.com>
2020-10-24 13:53:45 +0100

    Write tests for session.save

--- __tests__/wares/session.test.ts
+++ __tests__/wares/session.test.ts
@@ -38,4 +38,107 @@ describe('getSession(req, res)', () => {
 
     await fetch('/').expect({ t: 1 })
   })
+
+  describe('session.save(cb)', () => {
+    it('should save session to store', async () => {
+      const store = new MemoryStore()
+
+      const getSession = SessionManager({
+        store,
+        secret: 'test',
+      })
+
+      const { fetch } = InitAppAndTest(async (req, res) => {
+        const session = await getSession(req, res)
+
+        session.save((err) => {
+          if (err) {
+            return res.end(err.message)
+          }
+
+          store.get(session.id, (err, sess) => {
+            if (err) {
+              return res.end(err.message)
+            }
+
+            res.end(sess ? 'stored' : 'empty')
+          })
+        })
+      })
+
+      await fetch('/').expectStatus(200).expectBody('stored')
+    })
+
+    it('should prevent end-of-request save', async () => {
+      const store = new MemoryStore()
+
+      const _set = store.set
+
+      let setCount = 0
+
+      store.set = function set(...args) {
+        setCount++
+
+        return _set.apply(this, args)
+      }
+
+      const getSession = SessionManager({
+        store,
+        secret: 'test',
+      })
+
+      const { fetch } = InitAppAndTest(async (req, res) => {
+        const session = await getSession(req, res)
+
+        session.save((err) => {
+          if (err) {
+            return res.end(err.message)
+          }
+
+          res.end('saved')
+        })
+      })
+
+      await fetch('/').expectStatus(200).expectBody('saved')
+
+      expect(setCount).toBe(1)
+    })
+
+    it('should prevent end-of-request save on reloaded session', async () => {
+      const store = new MemoryStore()
+
+      const _set = store.set
+
+      let setCount = 0
+
+      store.set = function set(...args) {
+        setCount++
+
+        return _set.apply(this, args)
+      }
+
+      const getSession = SessionManager({
+        store,
+        secret: 'test',
+      })
+
+      const { fetch } = InitAppAndTest(async (req, res) => {
+        const session = await getSession(req, res)
+
+        session.reload(() => {
+          session.save((err) => {
+            if (err) {
+              return res.end(err.message)
+            }
+
+            res.end('saved')
+          })
+        })
+      })
+
+      await fetch('/').expectStatus(200).expectBody('saved')
+
+      expect(setCount).toBe(1)
+    })
+  })
 })
