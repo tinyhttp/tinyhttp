5204719874289a7779c3ee2d7ab1851f71e29806
iketiunn <ike.chang@sideeffect.dev>
2020-10-26 11:42:58 +0800

    chore: get rid of @ts-ignore

--- examples/firebase-functions/src/index.ts
+++ examples/firebase-functions/src/index.ts
@@ -1,22 +1,29 @@
 import * as functions from 'firebase-functions'
-import { App } from '@tinyhttp/app'
+import { App, Request, Response } from '@tinyhttp/app'
 const tinyhttp = new App()
 
 /** Your application code */
+tinyhttp.get('/api', (req, res) => {
+  res.send({ ok: true, q: req.query })
+})
+tinyhttp.post('/api', (req, res) => {
+  res.send({ ok: true, b: req.query })
+})
 tinyhttp.use('/', (_, res) => {
   res.send('<h1>Hello World</h1>')
 })
 
+/** Make firebase function handler compatible with tinyhttp */
+const onRequest = (functions.https.onRequest as unknown) as (handler: (req: Request, res: Response) => void | Promise<void>) => functions.HttpsFunction
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
-export const app = functions.https.onRequest((request, response) => {
+export const app = onRequest((request, response) => {
   makeReadonlySettable(request)
-  // @ts-ignore
-  tinyhttp.handler(request, response).catch(() => response.send('sad'))
+  tinyhttp.handler(request, response)
 })
 
 /** Hack for firebase functions request object, it was read only */
-function makeReadonlySettable(req: functions.https.Request) {
+function makeReadonlySettable(req: Request) {
   return ['xhr', 'path'].forEach((key) => {
     Object.defineProperty(req, key, {
       get: function () {
