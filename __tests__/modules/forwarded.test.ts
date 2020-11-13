3391e23378255eceb52b8500fb3c8b52979ad9d5
v1rtl <pilll.pl22@gmail.com>
2020-11-13 12:11:36 +0300

    test: add tests for forwarded

---
+++ __tests__/modules/forwarded.test.ts
@@ -0,0 +1,25 @@
+import { forwarded } from '../../packages/forwarded/src'
+import { createReq } from '../../test_helpers/createReq'
+import { IncomingMessage } from 'http'
+
+describe('forwarded(req)', () => {
+  it('should work with `X-Forwarded-For` header', () => {
+    const req = createReq('127.0.0.1') as IncomingMessage
+
+    expect(forwarded(req)).toEqual(['127.0.0.1'])
+  })
+  it('should include entries from `X-Forwarded-For`', () => {
+    const req = createReq('127.0.0.1', {
+      'x-forwarded-for': '10.0.0.2, 10.0.0.1',
+    }) as IncomingMessage
+
+    expect(forwarded(req)).toEqual(['127.0.0.1', '10.0.0.1', '10.0.0.2'])
+  })
+  it('should skip blank entries', () => {
+    const req = createReq('127.0.0.1', {
+      'x-forwarded-for': ' 10.0.0.2 ,  , 10.0.0.1 ',
+    }) as IncomingMessage
+
+    expect(forwarded(req)).toEqual(['127.0.0.1', '10.0.0.1', '10.0.0.2'])
+  })
+})
