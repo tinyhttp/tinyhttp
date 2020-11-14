ad2d837eae375925138e5114dbb90c955cee8695
v1rtl <pilll.pl22@gmail.com>
2020-11-15 00:47:31 +0300

    chore: upgrade pkgs in root

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-
   collectCoverageFrom: ['packages/**/src/*.{js,ts}', '!/node_modules/', '!examples/*.{js,ts}'],
   modulePathIgnorePatterns: ['examples', '/node_modules/'],
 }
