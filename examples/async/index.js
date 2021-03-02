95e8c5e2b240764107bf9502095fa4a86d2018b9
v 1 r t l <pilll.PL22@gmail.com>
2021-03-02 19:25:17 +0300

    Update index.js

--- examples/async/index.js
+++ examples/async/index.js
@@ -4,7 +4,9 @@ import { readFile } from 'fs/promises'
 const app = new App()
 
 app.get('/', async (_, res) => {
-  res.sendFile(`${process.cwd()}/test.txt`)
+  const file = await readFile(`${process.cwd()}/test.txt`)
+  
+  res.send(file.toString())
 })
 
 app.listen(3000, () => console.log('Started on http://localhost:3000'))
