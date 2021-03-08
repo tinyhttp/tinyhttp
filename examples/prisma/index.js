d2f3d937b0eff76d7274abca1b0be9f6e016f2a9
v1rtl <pilll.pl22@gmail.com>
2021-03-08 17:05:23 +0300

    feat(examples): update prisma example

--- examples/prisma/index.js
+++ examples/prisma/index.js
@@ -6,25 +6,18 @@ const prisma = new Prisma.PrismaClient()
 const app = new App()
 
 app.use(bodyParser.json())
-app.post(`/user`, handlePostUser)
-app.post(`/post`, handlePostPost)
-app.put('/publish/:id', handlePutPublishById)
-app.delete(`/post/:id`, handleDeletePostById)
-app.get(`/post/:id`, handleGetPostById)
-app.get('/feed', handleGetFeed)
-app.get('/filterPosts', handleGetFilterPosts)
 
-async function handlePostUser(req, res) {
-  const result = await prisma.user.create({
-    data: {
-      email: req.body.email,
-      name: req.body.name
-    }
-  })
-  res.json(result)
-}
-
-async function handlePostPost(req, res) {
+app.post(`/user`, async (req, res) => {
+  res.json(
+    await prisma.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.name
+      }
+    })
+  )
+})
+app.post(`/post`, async (req, res) => {
   const { title, content, authorEmail } = req.body
   const result = await prisma.post.create({
     data: {
@@ -35,64 +28,62 @@ async function handlePostPost(req, res) {
     }
   })
   res.json(result)
-}
-
-async function handlePutPublishById(req, res) {
-  const { id } = req.params
-  const post = await prisma.post.update({
-    where: { id: Number(id) },
-    data: { published: true }
-  })
-  res.json(post)
-}
-
-async function handleDeletePostById(req, res) {
-  const { id } = req.params
-  const post = await prisma.post.delete({
-    where: {
-      id: Number(id)
-    }
-  })
-  res.json(post)
-}
-
-async function handleGetPostById(req, res) {
-  const { id } = req.params
-  const post = await prisma.post.findOne({
-    where: {
-      id: Number(id)
-    }
-  })
-  res.json(post)
-}
-
-async function handleGetFeed(req, res) {
-  const posts = await prisma.post.findMany({
-    where: { published: true },
-    include: { author: true }
-  })
-  res.json(posts)
-}
-
-async function handleGetFilterPosts(req, res) {
+})
+app.put('/publish/:id', async (req, res) => {
+  res.json(
+    await prisma.post.update({
+      where: { id: Number(req.params.id) },
+      data: { published: true }
+    })
+  )
+})
+app.delete(`/post/:id`, async (req, res) => {
+  res.json(
+    await prisma.post.delete({
+      where: {
+        id: Number(req.params.id)
+      }
+    })
+  )
+})
+app.get(`/post/:id`, async (req, res) => {
+  res.json(
+    await prisma.post.findOne({
+      where: {
+        id: Number(req.params)
+      }
+    })
+  )
+})
+app.get('/feed', async (_, res) => {
+  res.json(
+    await prisma.post.findMany({
+      where: { published: true },
+      include: { author: true }
+    })
+  )
+})
+app.get('/filterPosts', async (req, res) => {
   const { searchString } = req.query
-  const draftPosts = await prisma.post.findMany({
-    where: {
-      OR: [
-        {
-          title: {
-            contains: searchString
+
+  res.json(
+    await prisma.post.findMany({
+      where: {
+        OR: [
+          {
+            title: {
+              contains: searchString
+            }
+          },
+          {
+            content: {
+              contains: searchString
+            }
           }
-        },
-        {
-          content: {
-            contains: searchString
-          }
-        }
-      ]
-    }
-  })
-  res.json(draftPosts)
-}
+        ]
+      }
+    })
+  )
+})
 
 app.listen(3000, () => console.log('Server ready at: http://localhost:3000'))
