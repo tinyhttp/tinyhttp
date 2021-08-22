463652342817361d96f9fd622fd595b29f34c641
v1rtl <pilll.pl22@gmail.com>
2021-08-22 12:43:52 +0300

    tooling: fix jest config

--- jest.config.mjs
+++ jest.config.mjs
@@ -2,6 +2,7 @@ export default {
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
   collectCoverage: true,
+  collectCoverageFrom: ['packages/**/src/*.{js,ts}', '!/node_modules/', '!examples/*.{js,ts}'],
   preset: 'ts-jest/presets/default-esm',
   globals: {
     'ts-jest': {
@@ -14,5 +15,5 @@ export default {
   testRegex: '(/tests/.*|\\.(test))\\.(ts|tsx|js)$',
   moduleFileExtensions: ['ts', 'js', 'json'],
   resolver: './resolver.cjs',
-  modulePathIgnorePatterns: ['tests/wares/rate-limit/rate-limit.test.ts', 'uvu_tests', 'examples', 'site']
+  modulePathIgnorePatterns: ['tests/wares/rate-limit/rate-limit.test.ts', 'examples']
 }
