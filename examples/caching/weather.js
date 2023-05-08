5da3f713f9c7ce66cdb70f4717a5e37d74d188eb
aarontravass <aaron.travass@hotmail.com>
2023-05-08 14:55:11 +0000

    fix: removed axios and replaced it with native fetch in caching example

--- examples/caching/weather.js
+++ examples/caching/weather.js
@@ -1,8 +1,6 @@
-import axios from 'axios'
-
 const URL = 'https://goweather.herokuapp.com/weather/'
 
 export async function getWeatherByCity(city) {
-  const result = await axios.get(URL + city)
-  return result.data
+  const result = await fetch(URL + city)
+  return result.json()
 }
