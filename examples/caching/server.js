a8a2a15a665ac6a748a4d2cce6a919ebc3193d2e
aarontravass <aaron.travass@hotmail.com>
2023-05-09 19:51:19 +0000

    fix: removed cache-manager and used lru-send instead

--- examples/caching/server.js
+++ examples/caching/server.js
@@ -1,23 +1,14 @@
 import { App } from '@tinyhttp/app'
-import { get, set } from './cache.js'
 import { getWeatherByCity } from './weather.js'
+import { lruSend } from 'lru-send'
 
-const app = new App()
+const PORT = 3000
+const app = new App().use(lruSend())
 
 app.get('/weather/search', async (req, res) => {
-  const t1 = new Date().getMilliseconds()
   const city = req.query.city
-  let cacheResult = await get(city)
-  if (cacheResult == null) {
-    console.log('Cache miss!')
-    cacheResult = await getWeatherByCity(city)
-    await set(city, cacheResult, 1000000) // passive caching strategy
-  } else console.log('Cache hit!')
-  const elapsed_time = new Date().getMilliseconds() - t1
-  return res.status(200).send({
-    elapsed_time,
-    ...cacheResult
-  })
+  const cacheResult = await getWeatherByCity(city)
+  return res.status(200).send(cacheResult)
 })
 
-app.listen(3000, () => console.log('Started listening on Port 3000'))
+app.listen(PORT, () => console.log(`Started listening on Port ${PORT}`))
