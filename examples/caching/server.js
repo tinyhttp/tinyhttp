1b47ef641b388b927f9bce046ef966510d732575
aarontravass <aaron.travass@hotmail.com>
2023-05-14 15:55:28 +0000

    fix: moved weather.js into server.js for caching example

--- examples/caching/server.js
+++ examples/caching/server.js
@@ -1,9 +1,14 @@
 import { App } from '@tinyhttp/app'
-import { getWeatherByCity } from './weather.js'
 import { lruSend } from 'lru-send'
 
+const URL = 'https://goweather.herokuapp.com/weather/'
 const PORT = 3000
 const app = new App().use(lruSend())
+
+async function getWeatherByCity(city) {
+  const result = await fetch(URL + city)
+  return result.json()
+}
 
 app.get('/weather/search', async (req, res) => {
   const city = req.query.city
--- examples/caching/weather.js
+++
@@ -1,6 +0,0 @@
-const URL = 'https://goweather.herokuapp.com/weather/'
-
-export async function getWeatherByCity(city) {
-  const result = await fetch(URL + city)
-  return result.json()
-}
