54d95f08fb6a2f8f5e7c017e5017be96bbeff6a6
v1rtl <yo@v1rtl.site>
2023-08-11 10:35:04 +0300

    chore: update examples

--- examples/cluster/index.js
+++ examples/cluster/index.js
@@ -2,7 +2,7 @@ import { App } from '@tinyhttp/app'
 import cluster from 'cluster'
 import os from 'os'
 
-if (cluster.isMaster) {
+if (cluster.isPrimary) {
   for (let i = 0; i < os.cpus().length; i++) {
     cluster.fork()
   }
