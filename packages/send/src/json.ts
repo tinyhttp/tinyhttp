e5dc4b1b6b1bf7f585a764984974f7cafc2314e4
Jan-T. Brinkmann <JTBrinkmann@users.noreply.github.com>
2024-04-02 11:33:20 +0200

    fix: res.json() accept non-object input

--- packages/send/src/json.ts
+++ packages/send/src/json.ts
@@ -10,13 +10,12 @@ export const json =
   <Response extends Res = Res>(res: Response) =>
   (body: any, ...args: any[]): Response => {
     res.setHeader('Content-Type', 'application/json')
-    if (typeof body === 'object' && body != null) res.end(JSON.stringify(body, null, 2), ...args)
-    else if (typeof body === 'string') res.end(body, ...args)
-    else if (body == null) {
+    if (body == null) {
       res.removeHeader('Content-Length')
       res.removeHeader('Transfer-Encoding')
       res.end(null, ...args)
-    }
+    } else if (typeof body === 'string') res.end(body, ...args)
+    else res.end(JSON.stringify(body, null, 2), ...args) 
 
     return res
   }
